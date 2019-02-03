31fc6dde248b2dd8aafa2c2e07fe5d85956af0b5
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2019-02-03 13:51:37 +0200

    Add stepper motor support

--- test.cpp
+++ test.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 //#include <unistd.h>
 //#include <sys/select.h>
 //#include <sys/stat.h>
@@ -6,15 +5,19 @@
 //#include <string.h>
 //#include <termios.h>
 //#include <stdlib.h>
-//#include <wiringPi.h>
 
 #include "HostPty.h"
+#include "SpeedyStepper.h"
+
+#include <wiringPi.h>
+#include <iostream>
 
 int main(int argc, char** argv)
 {
-#if 0
     if (wiringPiSetupGpio () == -1)
         return 1;
+
+#if 0
 
     pinMode(5, OUTPUT);
     pinMode(6, OUTPUT);
@@ -40,11 +43,24 @@ int main(int argc, char** argv)
     }
 #endif
 
+    const float STEPS_PER_MM = 200*16/1; //steps per revolution * microstepping / mm per revolution
+    const float HIGH_SPEED = 2;
+    const float HIGH_ACCELERATION = 1;
+
+    SpeedyStepper stepper;
+    stepper.connectToPins(6, 5);
+    stepper.setStepsPerMillimeter(STEPS_PER_MM);
+    stepper.setSpeedInMillimetersPerSecond(HIGH_SPEED);
+    stepper.setAccelerationInMillimetersPerSecondPerSecond(HIGH_ACCELERATION);
+
+
     HostPty pty("/dev/ttyNanoDLP");
     while(1)
     {
         std::string str = pty.nextString();
         std::cout << "Received line: " << str << std::endl;
+
+        stepper.moveRelativeInMillimeters(5);
     }
 
     return 0;
