9545bac14826eaf7f07ab6cc2230e80b414689a5
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2019-02-04 22:36:04 +0200

    Add buttons support for z-axis movement

---
+++ Config.h
@@ -0,0 +1,65 @@
+#pragma once
+
+//////// Z-axis parameters ///////////////
+//////////////////////////////////////////
+
+// Pin #s where motor is connected
+const int DIR_PIN = 5;
+const int STEP_PIN = 6;
+const int ENABLE_PIN = 12;
+
+// Steps in 1mm along Z axis
+const float STEPS_PER_MM = 400*16/8; //steps per revolution * microstepping / mm per revolution
+
+
+// Enable this option if the printer has hardware up/down button.
+#define SUPPORT_UP_DOWN_BUTTONS 1
+
+#if SUPPORT_UP_DOWN_BUTTONS
+// a single z-axis movement using HW buttons 
+const float MANUAL_MOVEMENT_MM = 0.1; //mm
+
+// After a few single moves Z-axis will move with low speed/acceleration
+const float LOW_SPEED = 2; // mm/s
+const float LOW_ACCELERATION = 5; // mm/s2
+
+// If pressing up or down button for >5 seconds z-axis will move faster
+const float HIGH_SPEED = 8; // mm/s
+const float HIGH_ACCELERATION = 20; // mm/s2
+
+// Pin #s for up/down buttons
+const int UP_BTN_PIN = 26;
+const int DOWN_BTN_PIN = 19;
+
+// Pull up/down modes for up/down buttons (see PUD_UP/PUD_DOWN/PUD_OFF values)
+const int UP_BTN_PUD = 1; // PUD_DOWN
+const int DOWN_BTN_PUD = 1; // PUD_DOWN
+
+#endif //SUPPORT_UP_DOWN_BUTTONS
+
+
+#if 0
+
+#ifdef JAP_PRINTER
+
+const int LED_ON_BTN_PIN = A2;
+
+const int UV_LED_PIN = A6;
+
+#else // Wanhao D7
+
+const int DIR_PIN = 48;
+const int STEP_PIN = 76;
+const int ENABLE_PIN = A8;
+
+const int LED_ON_BTN_PIN = 18;
+
+const int UV_LED_PIN = 9;
+#endif//
+
+
+const int CMD_TIMEOUT = 5000;
+const int BUF_LEN = 100;
+
+
+#endif //0
---
+++ NanoDLPShield.cpp
@@ -0,0 +1,121 @@
+#include "HostPty.h"
+#include "SpeedyStepper.h"
+#include "Config.h"
+
+#include <wiringPi.h>
+#include <iostream>
+
+SpeedyStepper stepper;
+
+void setSteperLowSpeed()
+{
+    stepper.setSpeedInMillimetersPerSecond(LOW_SPEED);
+    stepper.setAccelerationInMillimetersPerSecondPerSecond(LOW_ACCELERATION);
+}
+
+void setSteperHighSpeed()
+{
+    stepper.setSpeedInMillimetersPerSecond(HIGH_SPEED);
+    stepper.setAccelerationInMillimetersPerSecondPerSecond(HIGH_ACCELERATION);
+}
+
+bool isButtonPressed(int btnPin)
+{
+    return digitalRead(btnPin) == HIGH;
+}
+
+void processBtnMovement(int btnPin, int direction = 1)
+{
+    // Try small movements first
+    setSteperHighSpeed();
+    for(int i=0; i<5; i++)
+    {
+        stepper.moveRelativeInMillimeters(MANUAL_MOVEMENT_MM * direction);
+        delay(300);
+        if(!isButtonPressed(btnPin))
+            return;
+    }
+
+    // Then move at low speed for 3 sec
+    setSteperLowSpeed();
+    unsigned long startTime = millis();
+    while(millis() < startTime + 3000)
+    {
+        if(stepper.motionComplete())
+            stepper.setupRelativeMoveInMillimeters(1000 * direction);
+        else
+            stepper.processMovement();
+
+        // Check if button released
+        if(!isButtonPressed(btnPin))
+        {
+            stepper.setupStop();
+            while(!stepper.processMovement())
+                ;
+            return;
+        }
+    }
+
+    // Then move at high speed
+    setSteperHighSpeed();
+    stepper.setupRelativeMoveInMillimeters(1000 * direction);
+    while(isButtonPressed(btnPin))
+    {
+        if(stepper.motionComplete())
+            stepper.setupRelativeMoveInMillimeters(1000 * direction);
+        else
+            stepper.processMovement();
+    }
+
+    // Stop when button released
+    stepper.setupStop();
+    while(!stepper.processMovement())
+        ;
+}
+
+void processMotorOnCmd() //M17
+{
+    digitalWrite(ENABLE_PIN, LOW);
+}
+
+void processMotorOffCmd() //M18
+{
+    digitalWrite(ENABLE_PIN, HIGH);
+}
+
+
+int main(int argc, char** argv)
+{
+    if (wiringPiSetupGpio () == -1)
+        return 1;
+
+    stepper.connectToPins(STEP_PIN, DIR_PIN);
+    stepper.setStepsPerMillimeter(STEPS_PER_MM);
+    setSteperHighSpeed();
+
+    pinMode(UP_BTN_PIN, INPUT);
+    pullUpDnControl(UP_BTN_PIN, UP_BTN_PUD);
+    pinMode(DOWN_BTN_PIN, INPUT);
+    pullUpDnControl(DOWN_BTN_PIN, DOWN_BTN_PUD);
+
+    HostPty pty("/dev/ttyNanoDLP");
+    while(1)
+    {
+        if(isButtonPressed(UP_BTN_PIN))
+        {
+            processMotorOnCmd();
+            processBtnMovement(UP_BTN_PIN, 1);
+        }
+
+        if(isButtonPressed(DOWN_BTN_PIN))
+        {
+            processMotorOnCmd();
+            processBtnMovement(DOWN_BTN_PIN, -1);
+        }
+
+//        std::string str = pty.nextString();
+//        std::cout << "Received line: " << str << std::endl;
+    }
+
+    return 0;
+}
